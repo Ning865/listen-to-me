248e0912334aa0f67f6a379e2ff70e4d87766729
AK47are <3318517070@qq.com>
2026-03-26 23:15:34 +0800

    feat(后端): 实现专门的 AuthService 类和其实现类

---
+++ src/main/java/com/github/listen_to_me/service/AuthService.java
@@ -0,0 +1,5 @@
+package com.github.listen_to_me.service;
+
+public interface AuthService {
+
+}
---
+++ src/main/java/com/github/listen_to_me/service/impl/AuthServiceImpl.java
@@ -0,0 +1,10 @@
+package com.github.listen_to_me.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.github.listen_to_me.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+}
