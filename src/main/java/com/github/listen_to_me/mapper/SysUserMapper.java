ea4d7d03a67e5525abaeb09f473acdd9aeb14cf2
AK47are <3318517070@qq.com>
2026-03-26 10:32:53 +0800

    feat(后端): 实现获取用户权限的 Mapper 方法

--- src/main/java/com/github/listen_to_me/mapper/SysUserMapper.java
+++ src/main/java/com/github/listen_to_me/mapper/SysUserMapper.java
@@ -1,16 +1,13 @@
 package com.github.listen_to_me.mapper;
 
-import com.github.listen_to_me.domain.entity.SysUser;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import java.util.List;
 
-/**
- * <p>
- *  Mapper 接口
- * </p>
- *
- * @author kun
- * @since 2026-03-24
- */
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.listen_to_me.domain.entity.SysUser;
+
 public interface SysUserMapper extends BaseMapper<SysUser> {
+
+    // 获取指定用户的权限情况
+    List<String> selectPermCodeListById(Long userId);
 
 }
