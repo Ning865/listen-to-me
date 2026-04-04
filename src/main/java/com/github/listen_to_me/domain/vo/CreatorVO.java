ffe583cd7c304fa469b0a60ad71a9dd832ef3c82
AK47are <3318517070@qq.com>
2026-04-04 12:10:33 +0800

    feat(后端): 实现 CreatorVO 类
    用于向用户端显示创作者信息

---
+++ src/main/java/com/github/listen_to_me/domain/vo/CreatorVO.java
@@ -0,0 +1,16 @@
+package com.github.listen_to_me.domain.vo;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class CreatorVO {
+    private Long creatorId;
+    private String nickname;
+    private String avatar;
+    private Long consultCount;
+    private Long audioCount;
+    private Long fansCount;
+    private BigDecimal minPrice;
+    private Boolean isFollowing;
+}
