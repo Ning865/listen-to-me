fd3f8664336caa91dd64a2ccf816112e0e2bf32c
hulunkunzao <2380381330@qq.com>
2026-04-06 15:44:22 +0800

    feat(后端): 创建 AudioOrderVO 类

---
+++ src/main/java/com/github/listen_to_me/domain/vo/AudioOrderVO.java
@@ -0,0 +1,16 @@
+package com.github.listen_to_me.domain.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class AudioOrderVO {
+    private String orderSn;
+    private String audioTitle;
+    private String coverUrl;
+    private BigDecimal payAmount;
+    private String status;
+    private LocalDateTime createTime;
+}
