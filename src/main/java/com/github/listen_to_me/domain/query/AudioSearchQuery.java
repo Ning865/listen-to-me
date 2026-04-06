5cfefa09336ef01ff7b0f564d41d292891ee90fc
Ning001 <2825601439@qq.com>
2026-04-06 19:15:09 +0800

    feat(后端): 封装 AudioSearchQuery 类

---
+++ src/main/java/com/github/listen_to_me/domain/query/AudioSearchQuery.java
@@ -0,0 +1,21 @@
+package com.github.listen_to_me.domain.query;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Schema(description = "音频搜索查询参数")
+public class AudioSearchQuery extends PageQuery{
+    @NotBlank(message = "搜索关键词不能为空")
+    private String keyword;
+
+    /**
+     * 搜索类型
+     * 枚举值: TITLE / CREATOR / TRANSCRIPT， 默认 TITLE
+     */
+    @NotBlank(message = "搜索类型不能为空")
+    private String searchType = "TITLE";
+}
